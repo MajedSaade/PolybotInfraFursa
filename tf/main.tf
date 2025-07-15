2c68717489a0e8032b0ef85868c5c2dea16d53f8
MajedSaade <majedsaade.2001@gmail.com>
2025-07-15 23:18:15 +0300

    FinalTaskV4 - adding Load Balancer And Ngnix

--- tf/main.tf
+++ tf/main.tf
@@ -58,4 +58,6 @@ module "k8s_cluster" {
   instance_profile_name = "k8s-worker-profile-${var.env}"
   account_id          = var.account_id
   domain_name               = var.domain_name
+  subdomain                = var.subdomain
+
 }
