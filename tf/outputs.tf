fe3937016c7444c0ff3f7ad6f5924d76751976e8
MajedSaade <majedsaade.2001@gmail.com>
2025-07-16 00:03:35 +0300

    FinalTaskV4 - adding Load Balancer And Ngnix

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,3 +1,7 @@
 output "control_plane_public_ip" {
   value = module.k8s_cluster.control_plane_public_ip
 }
+output "vpc_id" {
+  value = module.k8s_cluster.vpc_id
+  description = "The ID of the VPC used by the Kubernetes cluster"
+}
